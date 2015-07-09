3019205f40c90e4288227cd1167646d1b0bcfee1
Sam Darwin <samuel.d.darwin@gmail.com>
2015-07-09 19:09:57 +0100

    add name, then increment version

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ maintainer       "Wanelo.com"
 maintainer_email "ops@wanelo.com"
 license          "Apache 2.0"
 description      "Ohai plugin for getting a node's private IP address (RFC 1918)"
-
-version          "0.1.1"
+name             "ohai-private-ipaddress"
+version          "0.1.2"
 
 depends          "ohai"
